00c01148e4b1136161820827c94fd06be46fc070
Aditt5 <9gadityarahman@gmail.com>
2026-02-21 13:39:57 +0800

    Add files via upload

--- naze.js
+++ naze.js
@@ -1592,7 +1592,7 @@ module.exports = naze = async (naze, m, msg, store) => {
 			break
 			case 'tagall': {
 				if (!m.isGroup) return m.reply(mess.group)
-				if (!m.isAdmin) return m.reply(mess.admin)
+				if (!m.isAdmin && !isCreator && !isPremium) return m.reply(mess.admin)
 				if (!m.isBotAdmin) return m.reply(mess.botAdmin)
 				let setv = pickRandom(listv)
 				let teks = `*Tag All*\n\n*Pesan :* ${q ? q : ''}\n\n`
@@ -1604,14 +1604,14 @@ module.exports = naze = async (naze, m, msg, store) => {
 			break
 			case 'hidetag': case 'h': {
 				if (!m.isGroup) return m.reply(mess.group)
-				if (!m.isAdmin) return m.reply(mess.admin)
+				if (!m.isAdmin && !isCreator && !isPremium) return m.reply(mess.admin)
 				if (!m.isBotAdmin) return m.reply(mess.botAdmin)
 				await m.reply(q ? q : '', { mentions: m.metadata.participants.map(a => a.id) })
 			}
 			break
 			case 'totag': {
 				if (!m.isGroup) return m.reply(mess.group)
-				if (!m.isAdmin) return m.reply(mess.admin)
+				if (!m.isAdmin && !isCreator && !isPremium) return m.reply(mess.admin)
 				if (!m.isBotAdmin) return m.reply(mess.botAdmin)
 				if (!m.quoted) return m.reply(`Reply pesan dengan caption ${prefix + command}`)
 				delete m.quoted.chat
